ec69fc8450e967a6adc1ec094c553f64bafafb35
Adam Bayu Satriaji <113441509+111202012556@users.noreply.github.com>
2022-10-03 02:05:05 +0700

    Add files via upload

---
+++ Pertemuan 1/debugger (2).cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugger : MonoBehaviour
+{
+    int x = 28;
+    public int z = 10;
+    public string name;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("Hello, my name is " + name);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("test");
+    }
+}
---
+++ Pertemuan 1/debugger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugger : MonoBehaviour
+{
+    int x = 28;
+    public int z = 10;
+    public string name; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debut.Log("Hello, my name is " + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
